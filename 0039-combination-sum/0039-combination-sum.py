ecfe9e721c99c490ad3104e21ab18b8e581cb6c4
Albertzry <125354474+Albertzry@users.noreply.github.com>
2024-08-20 19:02:28 +0800

    Update 0039-combination-sum.py

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -19,5 +19,22 @@ class Solution(object):
         temp = []
         backtrack(0, temp)
         return result
-            
+#下面是另外一种回溯，不用pop
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        ans = []
+        def find(s, use, remain):    #s表示起始起始位置，use表示已经添加的数列，remain表示剩余
+            for i in range(s, len(candidates)):
+                c = candidates[i]
+                if c == remain:
+                    ans.append(use + [c])
+                    return
+                if c < remain:
+                    find(i, use + [c], remain - c)
+                if c > remain:
+                    return
+        find(0, [], target)
+        return ans
+
             
