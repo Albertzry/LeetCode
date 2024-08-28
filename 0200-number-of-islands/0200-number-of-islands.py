7fb114f6eb7e34ed94da22b465dcb544bdb96f28
Albertzry <125354474+Albertzry@users.noreply.github.com>
2024-08-28 15:57:10 +0800

    Time: 194 ms (94.85%), Space: 27 MB (65.02%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,3 +1,4 @@
+#bfs
 class Solution(object):
     def numIslands(self, grid):
         """
@@ -23,3 +24,27 @@ class Solution(object):
                     bfs(i, j)
                     result+=1
         return result
+#dfs
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        m, n = len(grid), len(grid[0])
+        def dfs(i,j):
+            if not 0<=i<m or not 0<=j<n or grid[i][j]!="1":
+                return
+            grid[i][j]="0"
+            dfs(i-1,j)
+            dfs(i,j-1)
+            dfs(i+1,j)
+            dfs(i,j+1)
+        result=0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]=="1":
+                    dfs(i,j)
+                    result+=1
+        return result
+        
