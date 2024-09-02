b9c5e637febd5bf8d92a71e7688c519e7e8a0ff1
Albertzry <125354474+Albertzry@users.noreply.github.com>
2024-09-02 14:21:38 +0800

    Time: 274 ms (50%), Space: 13.9 MB (64.81%) - LeetHub

---
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxConsecutiveAnswers(self, answerKey, k):
+        """
+        :type answerKey: str
+        :type k: int
+        :rtype: int
+        """
+        def maxConsecutiveChar(ch):
+            ans, left, sum = 0, 0, 0
+            for right in range(len(answerKey)):
+                sum += answerKey[right] != ch
+                while sum > k:
+                    sum -= answerKey[left] != ch
+                    left += 1
+                ans = max(ans, right - left + 1)
+            return ans
+        return max(maxConsecutiveChar('T'), maxConsecutiveChar('F'))
