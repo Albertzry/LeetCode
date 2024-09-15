608878540874f471e031b02d37a472d3a0866412
Albertzry <125354474+Albertzry@users.noreply.github.com>
2024-09-15 11:02:58 +0800

    Time: 69 ms (85.54%), Space: 16.5 MB (50.82%) - LeetHub

---
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numberOfPoints(self, nums: List[List[int]]) -> int:
+        from itertools import accumulate
+        m = 102
+        d = [0] * m
+        for start, end in nums:
+            d[start] += 1
+            d[end + 1] -= 1
+        return sum(s > 0 for s in accumulate(d))
