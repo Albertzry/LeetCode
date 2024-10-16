f53b55b7b08fcf9543f6b62320f4fc8a08caf18c
Albertzry <125354474+Albertzry@users.noreply.github.com>
2024-10-16 11:19:59 +0800

    Time: 8 ms (95.08%), Space: 11.4 MB (96.72%) - LeetHub

---
+++ 1304-longest-happy-string/1304-longest-happy-string.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def longestDiverseString(self, a, b, c):
+        """
+        :type a: int
+        :type b: int
+        :type c: int
+        :rtype: str
+        """
+        ans = []
+        cnt = [[a, 'a'], [b, 'b'], [c, 'c']]
+        while True:
+            cnt.sort(key=lambda x: -x[0])
+            hasNext = False
+            for i, (c, ch) in enumerate(cnt):
+                if c <= 0:
+                    break
+                if len(ans) >= 2 and ans[-2] == ch and ans[-1] == ch:
+                    continue
+                hasNext = True
+                ans.append(ch)
+                cnt[i][0] -= 1
+                break
+            if not hasNext:
+                return ''.join(ans)
