03765377f308fa114f48259d18ec980530c2bf43
Albertzry <125354474+Albertzry@users.noreply.github.com>
2024-11-10 10:44:22 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
+         ans = inf
+        d = dict() 
+        for i, x in enumerate(nums):
+            d = {or_ | x: left for or_, left in d.items()}
+            d[x] = i  
+            for or_, left in d.items():
+                if or_ >= k:
+                    ans = min(ans, i - left + 1)
+        return ans if ans < inf else -1
