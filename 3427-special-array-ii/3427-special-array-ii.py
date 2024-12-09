494cdbfecd7dc48deea218923d08565188f7a59b
Albertzry <125354474+Albertzry@users.noreply.github.com>
2024-12-09 10:52:13 +0800

    Time: 31 ms (93.98%), Space: 46.6 MB (53.04%) - LeetHub

---
+++ 3427-special-array-ii/3427-special-array-ii.py
@@ -0,0 +1,5 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        s = list(accumulate((x % 2 == y % 2 for x, y in pairwise(nums)), initial=0))
+        return [s[from_] == s[to] for from_, to in queries]
+
