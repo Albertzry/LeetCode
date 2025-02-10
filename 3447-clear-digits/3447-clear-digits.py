00cc5f2e0045cde42a88e7ead48f0dc59367cfb5
Albertzry <125354474+Albertzry@users.noreply.github.com>
2025-02-10 08:56:49 +0800

    Time: 0 ms (100%), Space: 17.6 MB (70.7%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.py
+++ 3447-clear-digits/3447-clear-digits.py
@@ -1,20 +1,10 @@
-class Solution(object):
-    def clearDigits(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        s = list(s)
-        i,n=0,len(s)
-        while i<n:
-            if '0'<=s[i]<='9':
-                s.pop(i)
-                if i>0 and 'a'<=s[i-1]<='z':
-                    s.pop(i-1)
-                    i-=1
-                n=len(s)
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        res = []
+        for c in s:
+            if c.isdigit():
+                res.pop()
             else:
-                i+=1
-            
-        return ''.join(s)
-        
+                res.append(c)
+        return ''.join(res)
+
