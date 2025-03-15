1785ccaa3de245a3409d8048a61539b05aa8468f
Albertzry <125354474+Albertzry@users.noreply.github.com>
2025-03-15 19:39:31 +0800

    Time: 21 ms (70.94%), Space: 60.9 MB (67.69%) - LeetHub

--- 2690-house-robber-iv/2690-house-robber-iv.cpp
+++ 2690-house-robber-iv/2690-house-robber-iv.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     int minCapability(vector<int>& nums, int k) {
-         int lower = *min_element(nums.begin(), nums.end());
-        int upper = *max_element(nums.begin(), nums.end());
-        while (lower <= upper) {
-            int middle = (lower + upper) / 2;
-            int count = 0;
-            bool visited = false;
-            for (int x : nums) {
-                if (x <= middle && !visited) {
-                    count++;
-                    visited = true;
-                } else {
-                    visited = false;
+        int lower=*min_element(nums.begin(),nums.end());
+        int upper=*max_element(nums.begin(),nums.end());
+        while (lower<=upper){
+            int cnt=0;
+            int middle =(lower+upper)/2;
+            bool judge=false;
+            for (int x : nums){
+                if(x<=middle&&judge==false){
+                    cnt++;
+                    judge=true;
+                }
+                else{
+                    judge=false;
                 }
             }
-            if (count >= k) {
-                upper = middle - 1;
-            } else {
-                lower = middle + 1;
+            if (cnt>=k){
+                upper=middle-1;
+            }
+            else{
+                lower=middle+1;
             }
         }
         return lower;
