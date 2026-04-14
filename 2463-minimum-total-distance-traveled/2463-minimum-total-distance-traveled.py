63ac1ad19d7daf90120b66c3f03acc21846a3ff0
Albert R. Zhu <Albertzry@gmail.com>
2026-04-14 10:21:32 +0800

    Time: 595 ms (69.14%), Space: 19.8 MB (81.48%) - LeetHub

---
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumTotalDistance(self, robot: List[int], factory: List[List[int]]) -> int:
+        robot.sort()
+        factory.sort()
+        n, m = len(robot), len(factory)
+        dp = [[2**62 for _ in range(m + 1)] for _ in range(n + 1)]
+        for j in range(m + 1):
+            dp[0][j] = 0
+        for j in range(1, m + 1):
+            for i in range(1, n + 1):
+                dp[i][j] = dp[i][j - 1]
+                cost = 0
+                for k in range(1, min(i, factory[j - 1][1]) + 1):
+                    cost += abs(robot[i - k] - factory[j - 1][0])
+                    dp[i][j] = min(dp[i][j], dp[i - k][j - 1] + cost)
+        return dp[n][m]
