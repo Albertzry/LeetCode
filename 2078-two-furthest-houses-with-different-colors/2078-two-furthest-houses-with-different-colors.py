a19467e06d961a439fdb54906df07abdc0772ae6
Albert R. Zhu <Albertzry@gmail.com>
2026-04-20 09:55:43 +0800

    Time: 0 ms (100%), Space: 19.5 MB (6.45%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
@@ -3,11 +3,16 @@ class Solution:
         n = len(colors)
         l,r = 0,n-1
         dis = 0
+        visited = set()
         while l<n:
+            if colors[l] in visited:
+                l+=1
+                continue
             r=n-1
             while r>=0:
                 if colors[r]!=colors[l]:
                     dis = max(dis,r-l)
+                    visited.add(colors[l])
                     break
                 r-=1
             l+=1
